3c65fbadf320453b5a8bef013afc2705818ceaf5
Manisha <mpaswale@gmail.com>
2022-11-03 16:13:07 +0530

    stash file

--- Aarush.java
+++ Aarush.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 int a=20;
 int b=30;
@@ -5,3 +6,17 @@ int b=30;
 int c=a+b;
 
 syso(c);
+=======
+public class Aarush{
+
+public static void main(String[] args) {
+
+
+
+}
+
+
+
+
+}
+>>>>>>> Stashed changes
